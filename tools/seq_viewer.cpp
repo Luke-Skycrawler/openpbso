ca5d1c75b7aa5d335781bcfd5906dbe997dda277
shay <shay@utah.edu>
2026-01-09 13:14:43 -0700

    visualize flower abd sequence

--- tools/seq_viewer.cpp
+++ tools/seq_viewer.cpp
@@ -1,3 +1,6 @@
+#include <string>
+#include <vector>
+#include <memory>
 #include "igl/opengl/glfw/Viewer.h"
 #include "portaudio.h"
 #include "modal_solver.h"
@@ -7,18 +10,52 @@
 #include "ModalMaterial.h"
 #include "igl/read_triangle_mesh.h"
 #include "igl/per_vertex_normals.h"
-
-
 #include "igl/opengl/glfw/imgui/ImGuiMenu.h"
 
 /*------------------------------------
 Creates a libigl viewer for simulation sequence with sound synthesis
 *--------------------------------*/
 
-
+using namespace std;
+using scalar = double;
+using mat3 = Eigen::Matrix<scalar, 3, 3>;
+using vec3 = Eigen::Matrix<scalar, 3, 1>;
+using q4 = Eigen::Matrix<scalar, 3, 4>;
+struct AffineBody
+{
+    mat3 A = mat3::Zero();
+    vec3 p = vec3::Zero();
+    q4 q0 = q4::Zero(), dqdt = q4::Zero();
+    Eigen::MatrixXd deform(const Eigen::MatrixXd &V_rest)
+    {
+        return ((A * V_rest.transpose()).colwise() + p).transpose();
+    }
+};
 struct GlobalVariables {
-    int frame; 
-} globals; 
+    int frame, max_frame = 100;
+    vector<AffineBody> cubes;
+    Eigen::MatrixXd V_rest;
+} globals;
+
+void player_load(
+    const std::string &path,
+    int timestep,
+    vector<AffineBody> &cubes)
+{
+    string filename = path + "/" + to_string(timestep);
+    ifstream in(filename, ios::in | ios::binary);
+    int n_cubes = cubes.size();
+
+    for (int i = 0; i < n_cubes; i++)
+    {
+        auto &c = cubes[i];
+        in.read((char *)c.q0.data(), 12 * sizeof(scalar));
+        in.read((char *)c.dqdt.data(), 12 * sizeof(scalar));
+        c.p = c.q0.col(0);
+        c.A = c.q0.block<3, 3>(0, 1);
+    }
+    in.close();
+}
 
 int main(int argc, char** argv) {
     // create viewer
@@ -50,24 +87,32 @@ int main(int argc, char** argv) {
         ImGui::Begin("Simulation");
 
         ImGui::Checkbox("Animate", &viewer.core().is_animating);
-        ImGui::SliderInt("frame", &globals.frame, 0, 1000, "%d");
+        ImGui::SliderInt("frame", &globals.frame, 0, globals.max_frame, "%d");
         ImGui::End();
         
     };
 
     Eigen::MatrixXd V, VN; 
     Eigen::MatrixXi F;
-    igl::read_triangle_mesh("model_00000/00000.tet.obj", V, F);
+    igl::read_triangle_mesh("model_502009/flower.obj", V, F);
     igl::per_vertex_normals(V, F, VN);
-
+    globals.V_rest = V;
+    globals.cubes.resize(1);
     viewer.data().set_mesh(V, F);
 
     viewer.callback_pre_draw = [&](igl::opengl::glfw::Viewer &viewer)->bool {
         // update simulation frame
         if (viewer.core().is_animating) {
-            globals.frame = (globals.frame + 1) % 1000;
+            globals.frame = (globals.frame + 1) % globals.max_frame;
         }
 
+        player_load("trace", globals.frame, globals.cubes);
+        assert(globals.cubes.size() == 1);
+        for (auto &c : globals.cubes)
+        {
+            V = c.deform(globals.V_rest);
+            viewer.data(obj_id).set_vertices(V);
+        }
         return false;
     };
     viewer.launch(true, false, "Simulation Viewer");
